55cb5ba1d1e442800a253853ebb63a69a532b775
Rudolf.Mleziva <rudolf.mleziva@gmail.com>
2014-12-15 23:49:08 +0200

    Test

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -29,7 +29,21 @@ bool HelloWorld::init()
     
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	auto background = Sprite::create("Background.png");
+	background->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 
+	// add the sprite as a child to this layer
+	this->addChild(background, 6);
+	auto footer = Sprite::create("Footer.png");
+	Size footerSize = footer->getContentSize();
+	footer->setPosition(Vec2(visibleSize.width / 2 + origin.x, origin.y + footerSize.height/2 ));
+
+	// add the sprite as a child to this layer
+	this->addChild(footer, 7);
+
+	auto imagine = Sprite::create("iulia.jpg");
+    imagine->setPosition(Vec2(visibleSize.width / 2 + origin.x , visibleSize.height / 2 + origin.y));
+	this->addChild(imagine, 3);
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
@@ -38,10 +52,11 @@ bool HelloWorld::init()
     auto closeItem = MenuItemImage::create(
                                            "CloseNormal.png",
                                            "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+										   CC_CALLBACK_1(HelloWorld::puneAltBackground, this));
     
 	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
                                 origin.y + closeItem->getContentSize().height/2));
+	//closeItem->setScale(0.2f)
 
     // create menu, it's an autorelease object
     auto menu = Menu::create(closeItem, NULL);
@@ -76,14 +91,14 @@ bool HelloWorld::init()
 }
 
 
-void HelloWorld::menuCloseCallback(Ref* pSender)
+void HelloWorld::puneAltBackground(Ref* pSender)
 {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
 	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
     return;
 #endif
 
-    Director::getInstance()->end();
+   /* Director::getInstance()->end();*/
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
